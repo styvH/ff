79c0bf1dbee46a3c87b04da3d4b5f4bf80413ef5
styvH <100362675+styvH@users.noreply.github.com>
2022-05-02 13:02:09 -0400

    MetaMask on Laravel

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+        <button id="connectButton" onclick="bundle.connect()">Connect</button>
+        <button id="executeButton" onclick="bundle.execute()">Execute</button>
+
+        <script src=" {{ asset('js/metamask/dist/bundle.js')}}" type="text/javascript"></script>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-jet-welcome />
+            </div>
+        </div>
+    </div>
+</x-app-layout>
